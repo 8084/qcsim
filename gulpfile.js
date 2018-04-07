14b13e2f6bab451bb49561aedaf978b8c19dafeb
8084 <klntsky@gmail.com>
2018-04-07 15:49:05 +0300

    moved to gulp; moved js/bundle.js -> dist/bundle.js

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp')
+var browserify = require('browserify')
+var babelify = require('babelify')
+var watchify = require('watchify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+
+var b = function() {
+    return browserify({
+        cache: {},
+        packageCache: {},
+        entries: ['./js/main.js'],
+        debug: true,
+        transform: ['babelify']
+    });
+};
+
+var w = watchify(b());
+
+w.on('log', gutil.log);
+
+var bundle = function(pkg) {
+    return pkg.bundle()
+              .pipe(source('bundle.js'))
+              .pipe(gulp.dest('./dist'));
+};
+
+gulp.task('watch', function() {
+    bundle(w);
+    w.on('update', bundle.bind(null, w));
+});
+
+gulp.task('build', bundle.bind(null, b()));
+
+gulp.task('default', ['watch']);
