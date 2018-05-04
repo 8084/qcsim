b79eb42df2cb1787e96d07c478ced4c131dc9540
8084 <klntsky@gmail.com>
2018-05-04 17:07:17 +0300

    added: test for constructTT

--- test/zhegalkin.js
+++ test/zhegalkin.js
@@ -57,4 +57,17 @@ describe('zhegalkin.js', () => {
             assert.deepStrictEqual(z.evaluate(ast, args), res);
         });
     });
+
+    it('constructTT', () => {
+        for (var n = 1; n < 8; n++) {
+            var r = z.constructTT(n)
+                     .map(row => row
+                         .map(e => e ? '0':'1')
+                         .join(''));
+
+            var s = new Set(r);
+            assert.equal(s.size, Math.pow(2, n),
+                         "count of unique values returned by constructTT(n) is not equal to 2 ^ n, where n = " + n);
+        }
+    });
 });
